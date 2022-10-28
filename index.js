5fb88a169cd661ad770b46d4873a85fafc752b3f
Alan Pérez Fernández <a01640489@tec.mx>
2022-10-28 18:04:48 -0500

    Se modifico index.js

--- index.js
+++ index.js
@@ -11,11 +11,32 @@ app.on('before-quit', () => {
 //Ejecutando ordenes cuando la aplicación esta lista
 app.on('ready', () => {
     //creandu una nueva ventana
-    let win = new BrowserWindow()
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: 'Platzi Picture',
+        center: true,
+        maximizable : false,
+        show: false
+    })
+    //Este metodo muestra la ventana una vez que es cargada
+    win.once('ready-to-show',() => {
+        win.show()
+    })
+    //Metodo que obtiene e imprime la posición de la ventana en consola
+    /*
+    win.on('move',() =>{
+        const position = win.getPosition()
+        console.log(`la posición es ${position}`)
+    })
+    */
     // detectando el cierre de la ventana para cerrar el aplicativo
     win.on('closed', () => {
         win = null
         app.quit()
     })
+
+    win.loadURL(`file://${__dirname}/index.html`)
+
 })
 
